c65a9c3b3f5140b18feceb99389c1ca743b62a11
Chris Jensen <christophercjensen@gmail.com>
2011-06-08 16:23:58 -0700

    added example of how timers work

--- AndSensor/src/com/rimproject/andsensor/AccelerometerLogger.java
+++ AndSensor/src/com/rimproject/andsensor/AccelerometerLogger.java
@@ -8,6 +8,37 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 
+/* Example of how this class and its timer's will work:
+ * 
+ * Timer 1 -> 10 seconds has passed 
+ * 		Timer 2 created -> let me know when 3 seconds has passed
+ * 		Sensor recording start
+ * 			10:55:10 v:11
+ * 			10:55:10 v:12
+ * 			10:55:11 v:11
+ * 			10:55:11 v:10
+ * 			10:55:12 v:15
+ * 			10:55:12 v:16
+ * 			10:55:13 v:11
+ * 		Timer 2 -> 3 seconds has passed
+ * 		sensor recording stop
+ * 
+ * Timer 1 -> 10 seconds has passed 
+ * 		Timer 2 created -> let me know when 3 seconds has passed
+ * 		Sensor recording start
+ * 			10:55:20 v:11
+ * 			10:55:20 v:12
+ * 			10:55:21 v:11
+ * 			10:55:21 v:10
+ * 			10:55:22 v:15
+ * 			10:55:22 v:16
+ * 			10:55:23 v:11
+ * 		Timer 2 -> 3 seconds has passed
+ * 		sensor recording stop
+ * 	etc.
+ */
+
+
 public class AccelerometerLogger extends TimerTask implements SensorEventListener
 {
 	private Timer delayBetweenLoggingTimer; // Timer 1
