e9cbbef1fedd7123398ed4546503754bd7845fe8
Ibrahim ALShehri <ibrahim.h@gmail.com>
2011-06-08 11:48:22 -0700

    first commit

---
+++ AndSensor/src/com/rimproject/andsensor/SensorLogger.java
@@ -0,0 +1,16 @@
+package com.rimproject.andsensor;
+
+import java.util.HashMap;
+
+public class SensorLogger 
+{
+  HashMap listOfSensors = new HashMap();
+
+	public SensorLogger() 
+	{
+		super();
+		
+	}
+	  
+  
+}
