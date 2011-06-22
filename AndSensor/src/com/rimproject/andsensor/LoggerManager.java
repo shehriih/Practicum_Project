438de3d98c6d56aa4fd227bf5f72347bfe1c7bc4
Chris Jensen <christophercjensen@gmail.com>
2011-06-22 12:27:24 -0700

    activates all sensors

--- AndSensor/src/com/rimproject/andsensor/LoggerManager.java
+++ AndSensor/src/com/rimproject/andsensor/LoggerManager.java
@@ -10,8 +10,10 @@ public class LoggerManager
 	{
 		listOfSensors = new HashMap<String, BasicLogger>();
 		listOfSensors.put("Accelerometer", new AccelerometerLogger());
-		listOfSensors.put("Location", new GeoLocationLogger());
+		listOfSensors.put("GeoLocation", new GeoLocationLogger());
 		listOfSensors.put("LightSensor", new LightSensorLogger());
+		listOfSensors.put("NearbyWifi", new NearbyWifiLogger());
+		listOfSensors.put("NearbyBluetooth", new NearbyBluetoothLogger());
 	}
 	
 	public void initiateAllLogging()
