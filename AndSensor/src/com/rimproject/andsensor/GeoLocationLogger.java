fe5aba1c1d264f5548aa46f757cd2ad1b4163edf
Chris Jensen <christophercjensen@gmail.com>
2011-06-22 11:33:19 -0700

    fixed crash error (should consider changing this to use location tracking rather than bestKnownLastLocation)

--- AndSensor/src/com/rimproject/andsensor/GeoLocationLogger.java
+++ AndSensor/src/com/rimproject/andsensor/GeoLocationLogger.java
@@ -1,36 +1,32 @@
 package com.rimproject.andsensor;
 
+import java.util.Calendar;
+
 import android.location.Location;
 import android.location.LocationManager;
 
 public class GeoLocationLogger extends BasicLogger
 {
-	
 	private LocationManager locationManager;
 	private String locationSensor;
 	
 	public GeoLocationLogger() 
 	{
-		//this sensor is slightly different from the others. We only want a single recording
-		//as the OS keeps a rough track of the location for us.
 		super();
 		this.locationManager = (LocationManager) AndSensor.getContext().getSystemService(android.content.Context.LOCATION_SERVICE);
-		this.locationSensor = LocationManager.GPS_PROVIDER;//LocationManager.NETWORK_PROVIDER;
-		// Or use LocationManager.GPS_PROVIDER
-		setDelayBetweenLogging(60 * 1000);
+		this.locationSensor = /*LocationManager.GPS_PROVIDER;*/LocationManager.NETWORK_PROVIDER;
+		
+		setDelayBetweenLogging(5000);
 	}
 	
-	/*
-	 * we override the run method
-	 */
-	public void run() 
-	{
-		logLocation();
-		stopLogging();
-	}
-	
-	protected void logLocation() {
+	protected void performLogging() {
+		super.performLogging();
+		
 		Location lastKnownLocation = this.locationManager.getLastKnownLocation(this.locationSensor);
-		System.out.println(this+" logLocation: " + lastKnownLocation);
-	}
+		if (lastKnownLocation != null) {
+			super.writeToLogFile(lastKnownLocation.toString());
+		} else {
+			System.out.println(this+" performLogging failed");
+		}
+	}	
 }
