a9ba10a691d9d4aae25d1e4b90ba9c2b704237fe
Chris Jensen <christophercjensen@gmail.com>
2011-07-06 17:36:14 -0700

    sleep action detector performs calculation with placeholder values

--- AndSensor/src/com/rimproject/andsensor/ContextDetector.java
+++ AndSensor/src/com/rimproject/andsensor/ContextDetector.java
@@ -1,11 +1,27 @@
 package com.rimproject.andsensor;
 
 public class ContextDetector {
-	public static Boolean isSleeping() {
-		Boolean isSleeping = false;
+	public static double probabilityOfSleeping() {
+		final double timeWeighting = 0.4;
+		double timeScore = 1.0; //TODO: get score from sensor analyzer
+		double timeFactor = timeWeighting * timeScore;
 		
+		final double lightWeighting = 0.2;
+		double lightScore = 1.0; //TODO: get score from sensor analyzer
+		double lightFactor = lightWeighting * lightScore;
 		
+		final double deviceIsChargingWeighting = 0.1;
+		double deviceIsChargingScore = 1.0; //TODO: get score from sensor analyzer
+		double deviceIsChargingFactor = deviceIsChargingWeighting * deviceIsChargingScore;
 		
-		return isSleeping;
+		final double stationaryWeighting = 0.25;
+		double stationaryScore = 1.0; //TODO: get score from sensor analyzer
+		double stationaryFactor = stationaryWeighting * stationaryScore;
+		
+		final double deviceUseWeighting = 0.05;
+		double deviceUseScore = 1.0; //TODO: get score from sensor analyzer
+		double deviceUseFactor = deviceUseWeighting * deviceUseScore;
+		
+		return timeFactor + lightFactor + deviceIsChargingFactor + stationaryFactor + deviceUseFactor;
 	}
 }
