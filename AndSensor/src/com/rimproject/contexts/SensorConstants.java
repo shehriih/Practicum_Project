90ce890cf688baef124bf4930fbff3e46eae8396
Rahul Baxi <rahul.baxi@sv.cmu.edu>
2011-07-07 12:36:01 -0700

    Moved ContextDetector to rimprojects.contexts package

---
+++ AndSensor/src/com/rimproject/contexts/SensorConstants.java
@@ -0,0 +1,8 @@
+package com.rimproject.contexts;
+
+public class SensorConstants {
+	public static final String LOCATION_HOME = "home";
+	public static final String LOCATION_WORK = "work";
+	public static final double LIGHT_DIM = 10.0;
+	public static final int STATIONARY = 0;
+}
