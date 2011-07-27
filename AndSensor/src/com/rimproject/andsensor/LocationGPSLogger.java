2dd53f61dd2b1eaf32ffafe1a8de35e75e004552
Chris Jensen <christophercjensen@gmail.com>
2011-07-27 10:41:57 -0700

    clean up imports

--- AndSensor/src/com/rimproject/andsensor/LocationGPSLogger.java
+++ AndSensor/src/com/rimproject/andsensor/LocationGPSLogger.java
@@ -7,7 +7,6 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Looper;
 
 import com.rimproject.logreadings.LocationGPSReading;
 
